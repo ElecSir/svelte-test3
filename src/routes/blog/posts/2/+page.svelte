6d6079bdee855d70822d2d0cdf4b1ba609a255c0
Sergey <sterkhovsergey511@gmail.com>
2023-06-11 22:09:31 +0400

    add blog

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,3 @@
+<h1>My Second Blog Post</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, similique. Est amet voluptatum magnam ad architecto aliquid, tenetur maxime veniam quibusdam provident, nihil ducimus pariatur magni, asperiores aut voluptatem laudantium.</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus minus similique enim quod, incidunt modi sapiente doloribus ut veniam optio repellendus, sed recusandae, voluptatem error vitae vel dolorum magnam quaerat!</p>
